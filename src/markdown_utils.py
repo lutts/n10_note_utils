a8b2bb35d4b6ec8f859b309f1ab906318fb29d6f
Lutts <lutts.cao@gmail.com>
2022-09-24 22:29:12 +0800

    default body font-size to 12pt

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -65,6 +65,9 @@ class markdown_processor:
     css_style = """
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css" integrity="sha384-bYdxxUwYipFNohQlHt0bjN/LCpueqWz13HufFEV1SUatKs1cm4L6fFgCi1jT643X" crossorigin="anonymous">
         <style>
+        body {
+            font-size: 12pt;
+        }
         blockquote {
             margin-top: 10px;
             margin-bottom: 10px;
