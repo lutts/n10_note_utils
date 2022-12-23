85cbd1771e6e3475fe9814b3d323866305dd002f
Lutts <lutts.cao@gmail.com>
2022-12-23 20:36:23 +0800

    bold is not necessary in headers

--- src/html_to_markdown.py
+++ src/html_to_markdown.py
@@ -1,9 +1,11 @@
 
 import time
+import re
 from clipboard_utils import clipboard_util
 import keyboard
 from clipboard_monitor import py_clipboard_monitor
 from note_processor import RawNoteProcessor
+
 
 def copy_html_to_markdown():
     from markdownify import markdownify
@@ -20,4 +22,11 @@ def copy_html_to_markdown():
         md = markdownify(html, heading_style="ATX", strip=['a'])
         processor = RawNoteProcessor(md)
         processor.process()
-        clipboard_util.put_text(''.join(processor.markdown_lines))
+
+        header_re = re.compile(r'^#{1,6} ')
+        bold_re = re.compile(r'(\*\*)(.*?)(\*?)\1')
+
+        markdown_lines = [bold_re.sub(r'\2\3', line) if header_re.match(line) else line
+                          for line in processor.markdown_lines]
+
+        clipboard_util.put_text(''.join(markdown_lines))
